dad0bc32b13163b6cc265a310e6e477ee5aa4bc4
syedalihussain100 <syedalihussain100@gmail.com>
2020-09-07 17:00:47 +0500

    Saylani Es6

---
+++ App.js
@@ -0,0 +1,99 @@
+///////////////////////////Sayalni Es6//////////////////////////////
+
+// var name9;
+// setTimeout(function () {
+//   name9 = "Zaid";
+// }, 2000);
+
+// setInterval(function () {
+//   console.log(name9);
+// }, 1000);
+
+////////////// Promise ///////////////
+
+// const promise = new Promise(function (resolve, reject) {
+//   const zinger = "Avaible";
+
+//   if (zinger === "Avaible") {
+//     setTimeout(() => {
+//       resolve(
+//         "Takla Bhai nee Burger lee liya Saab kaa and Takla Bhai Zindabad"
+//       );
+//     }, 3000);
+//   } else {
+//     setTimeout(() => {
+//       reject("Taklee Nee Nahi Khelaya Burger Ap Takla koo Saab Maroo!!");
+//     }, 4000);
+//   }
+// });
+
+// promise
+//   .then(function (data) {
+//     console.log(`Resolve ===> ${data}`);
+//   })
+//   .catch(function (error) {
+//     console.log(`Rejected ===> ${error}`);
+//   });
+
+////      OBJECT DESTRUCTION ////////////////
+
+// let obj = {
+//   name: "Zaid Roy",
+//   email: "zaid@gmail.com",
+// };
+
+// let { name, email } = obj;
+
+// console.log(name);
+// console.log(email);
+
+/////////////////   Array  Destruction ///////////\
+
+// let arry = ["Hammad", "Farhan", "Zaid"];
+
+// let [name1, name2, name3] = arry;
+
+// console.log(name2);
+
+///// Arrow Function /////////
+
+// let hello = () => {
+//   console.log(this);
+// };
+
+// hello();
+
+// Array Function //////
+
+// let arr = [
+//   { name: "Zaid", age: 20 },
+//   { name: "Farhan", age: 30 },
+// ];
+
+// let filter = arr.filter(function (a) {
+//   return a.name === "Farhan";
+// });
+
+// console.log(filter);
+
+////////// Search ///////////////
+
+// let name = "Roy";
+// let search = "y";
+
+// if (name.endsWith(search)) {
+//   console.log(name);
+// }
+
+//////////// map ////////////
+
+// let array = [2, 3, 4, 5];
+// console.log(array);
+
+// let foo = array.map((a) => a * 2);
+
+// console.log(foo);
+
+/////////////////// Next Clasesss /////////////
+
+////////// Firebase Authentication //////////
